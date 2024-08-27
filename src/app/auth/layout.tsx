73b32260980678a7fdc4a865229b8ffcda285610
Ajmaldevala <ajmaldevala@gmail.com>
2024-08-27 17:36:14 +0530

    Auth page and layout page created

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
